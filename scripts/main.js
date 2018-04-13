daddeed03d9d7a4d4945152cea34b213de920ea7
DESKTOP-1HT7DM9\Dmytro <merlinultra@gmail.com>
2018-04-13 19:30:33 +0300

    Added animated scroll

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
     }
 
     $('.animation').AniView();
+
+    $('.scroll-element[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
 });
